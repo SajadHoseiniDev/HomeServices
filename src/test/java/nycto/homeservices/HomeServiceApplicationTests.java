2ac89669d6c4d5a8c596407b3a820d5293e28dff
Sajad <nyctoxr@gmail.com>
2025-03-15 01:24:51 +0330

    Setup Project with Spring Boot

---
+++ src/test/java/nycto/homeservices/HomeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nycto.homeservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
