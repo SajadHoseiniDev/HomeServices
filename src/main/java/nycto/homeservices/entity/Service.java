feb0da9877f86ee1847523da227e94137b6efc52
Sajad <nyctoxr@gmail.com>
2025-03-15 14:00:22 +0330

    Add Service class with name fields(I didn't configure any relations between entities yet!!!)

---
+++ src/main/java/nycto/homeservices/entity/Service.java
@@ -0,0 +1,23 @@
+package nycto.homeservices.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import nycto.homeservices.base.BaseEntity;
+
+@Entity
+
+@Getter
+@Setter
+@ToString
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public class Service extends BaseEntity {
+    @Column
+    String name;
+}
