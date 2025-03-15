9a1082758d7ed416e7bc588e28ed59bbda1b3300
Sajad <nyctoxr@gmail.com>
2025-03-15 13:52:19 +0330

    Add Specialist Class as a child of User Class with these custom fields: credit,profilePicUrl,rating

---
+++ src/main/java/nycto/homeservices/entity/Specialist.java
@@ -0,0 +1,28 @@
+package nycto.homeservices.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+
+@Getter
+@Setter
+@ToString
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Specialist extends User {
+    @Column
+    int rating;
+
+    @Column
+    String profilePicUrl;
+
+    @Column
+    int credit;
+
+}
