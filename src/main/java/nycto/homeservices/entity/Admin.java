001cd7cf92f44da1064d8b1c0cc59e298131efb8
Sajad <nyctoxr@gmail.com>
2025-03-15 13:53:40 +0330

    Add Admin Class as a child of User Class with no custom fields for now!!

---
+++ src/main/java/nycto/homeservices/entity/Admin.java
@@ -0,0 +1,19 @@
+package nycto.homeservices.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+
+@Getter
+@Setter
+@ToString
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Admin extends User {
+
+}
