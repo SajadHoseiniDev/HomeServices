0bba357b8614038e5b8cc172b15b609fe1965bf2
Sajad <nyctoxr@gmail.com>
2025-03-15 21:12:29 +0330

    feat: Add new entity [Order] and corresponding enum [OrderStatus]

---
+++ src/main/java/nycto/homeservices/entity/Order.java
@@ -0,0 +1,45 @@
+package nycto.homeservices.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import nycto.homeservices.base.BaseEntity;
+
+import java.time.LocalDateTime;
+
+@Entity
+
+@Getter
+@Setter
+@ToString
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Order extends BaseEntity {
+
+    @Column
+    Long proposedPrice;
+
+    @Column
+    String description;
+
+    @Column(nullable = false)
+    LocalDateTime orderDate;
+
+    @Column
+    LocalDateTime executionDate;
+
+    @Column(nullable = false)
+    String address;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    OrderStatus status;
+
+
+}
---
+++ src/main/java/nycto/homeservices/entity/OrderStatus.java
@@ -0,0 +1,12 @@
+package nycto.homeservices.entity;
+
+public enum OrderStatus {
+
+    WAITING_PROPOSALS,
+    WAITING_SPECIALIST,
+    WAITING_ARRIVAL,
+    STARTED,
+    DONE,
+    PAID
+
+}
