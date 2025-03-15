18c3a4006036e3e27729243e52e62edd6778b14f
Sajad <nyctoxr@gmail.com>
2025-03-15 13:56:00 +0330

    Add CustomerCredit class

---
+++ src/main/java/nycto/homeservices/entity/CustomerCredit.java
@@ -0,0 +1,25 @@
+package nycto.homeservices.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import nycto.homeservices.base.BaseEntity;
+
+@Entity
+
+@Getter
+@Setter
+@ToString
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public class CustomerCredit extends BaseEntity {
+
+    @Column
+    Long amount;
+
+}
