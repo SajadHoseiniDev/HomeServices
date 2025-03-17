9a35dbbd6deec6c9c40c668851c85d8ed3293cc6
Sajad <nyctoxr@gmail.com>
2025-03-18 00:19:00 +0330

    refactor: Remove CustomerCredit entity and repository and add credit field to Customer

--- src/main/java/nycto/homeservices/repository/CustomerCreditRepository.java
+++
@@ -1,9 +0,0 @@
-package nycto.homeservices.repository;
-
-import nycto.homeservices.entity.CustomerCredit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerCreditRepository extends JpaRepository<CustomerCredit, Long> {
-}
