f6873c3db67c80aa034458af5787ed8688c73c65
Sajad <nyctoxr@gmail.com>
2025-03-17 13:05:32 +0330

    feat: implement required validations fields  in UserDto

--- src/main/java/nycto/homeservices/dto/UserDto.java
+++ src/main/java/nycto/homeservices/dto/UserDto.java
@@ -1,6 +1,9 @@
 package nycto.homeservices.dto;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import nycto.homeservices.entity.enums.UserStatus;
 
 import java.time.LocalDateTime;
@@ -8,9 +11,23 @@ import java.time.LocalDateTime;
 public record UserDto(
 
         Long id
-        , String firstName
-        , String lastName
-        , String email
+
+        ,
+        @Min(3)
+        @Max(30)
+        @NotEmpty(message = "firstName can't be empty!")
+        String firstName
+
+        ,
+        @Min(3)
+        @Max(30)
+        @NotEmpty(message = "lastName can't be empty!")
+        String lastName
+
+        ,
+        @NotEmpty(message = "email can't be empty!")
+        @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "Invalid email format")
+        String email
         , UserStatus status
         , LocalDateTime registrationDate
 ) {}
