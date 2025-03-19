f07dbe898792b7e780ee3e100185e4b1b958edb7
Sajad <nyctoxr@gmail.com>
2025-03-19 23:25:15 +0330

    Create DTO for user filtering

---
+++ src/main/java/nycto/homeservices/dto/userDto/FilteringDto.java
@@ -0,0 +1,11 @@
+package nycto.homeservices.dto.userDto;
+
+import nycto.homeservices.entity.enums.UserType;
+
+public record FilteringDto(
+        String firstName,
+        String lastName,
+        String email,
+        UserType userType
+) {
+}
