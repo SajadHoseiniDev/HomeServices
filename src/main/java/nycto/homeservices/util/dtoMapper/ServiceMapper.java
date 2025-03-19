afd866d07d69b072a4cf3182210e4793f0923662
Sajad <nyctoxr@gmail.com>
2025-03-20 02:30:40 +0330

    Create mapper for SubService entity and Serivce entity

---
+++ src/main/java/nycto/homeservices/util/dtoMapper/ServiceMapper.java
@@ -0,0 +1,29 @@
+package nycto.homeservices.util.dtoMapper;
+
+import nycto.homeservices.dto.serviceDto.ServiceCreateDto;
+import nycto.homeservices.dto.serviceDto.ServiceResponseDto;
+import nycto.homeservices.dto.serviceDto.ServiceUpdateDto;
+import nycto.homeservices.entity.Service;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceMapper {
+
+    public Service toEntity(ServiceCreateDto createDto) {
+        Service service = new Service();
+        service.setName(createDto.name());
+        return service;
+    }
+
+    public Service toEntity(ServiceUpdateDto updateDto, Service existingService) {
+        existingService.setName(updateDto.name());
+        return existingService;
+    }
+
+    public ServiceResponseDto toResponseDto(Service service) {
+        return new ServiceResponseDto(
+                service.getId(),
+                service.getName()
+        );
+    }
+}
---
+++ src/main/java/nycto/homeservices/util/dtoMapper/SubServiceMapper.java
@@ -0,0 +1,40 @@
+package nycto.homeservices.util.dtoMapper;
+
+import nycto.homeservices.dto.subService.SubServiceCreateDto;
+import nycto.homeservices.dto.subService.SubServiceResponseDto;
+import nycto.homeservices.dto.subService.SubServiceUpdateDto;
+import nycto.homeservices.entity.Service;
+import nycto.homeservices.entity.SubService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubServiceMapper {
+
+    public SubService toEntity(SubServiceCreateDto createDto, Service service) {
+        SubService subService = new SubService();
+        subService.setName(createDto.name());
+        subService.setBasePrice(createDto.basePrice());
+        subService.setDescription(createDto.description());
+        subService.setService(service);
+        return subService;
+    }
+
+    public SubService toEntity(SubServiceUpdateDto updateDto, SubService existingSubService, Service service) {
+        existingSubService.setName(updateDto.name());
+        existingSubService.setBasePrice(updateDto.basePrice());
+        existingSubService.setDescription(updateDto.description());
+        existingSubService.setService(service);
+        return existingSubService;
+    }
+
+    public SubServiceResponseDto toResponseDto(SubService subService) {
+        return new SubServiceResponseDto(
+                subService.getId(),
+                subService.getName(),
+                subService.getBasePrice(),
+                subService.getDescription(),
+                subService.getService().getId()
+        );
+    }
+
+}
