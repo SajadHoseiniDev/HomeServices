58034256e57878d771d2ab3d66a19edc90a52e0c
Sajad <nyctoxr@gmail.com>
2025-03-20 02:25:54 +0330

    Create three DTOs for SubService entity: create,update,response.

--- src/main/java/nycto/homeservices/dto/subService/SubServiceResponseDto.java
+++ src/main/java/nycto/homeservices/dto/subService/SubServiceResponseDto.java
@@ -1,10 +1,5 @@
 package nycto.homeservices.dto.subService;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
 public record SubServiceResponseDto(
         Long id,
         String name,
