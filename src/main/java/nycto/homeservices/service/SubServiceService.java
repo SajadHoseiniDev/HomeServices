cb29e63b43d542cb475b9486552b45c131979c1f
Sajad <nyctoxr@gmail.com>
2025-03-20 09:41:23 +0330

    Implement getSubServiceById and getAllSubServices methods for SubServiceService.java

--- src/main/java/nycto/homeservices/service/SubServiceService.java
+++ src/main/java/nycto/homeservices/service/SubServiceService.java
@@ -4,11 +4,15 @@ import lombok.RequiredArgsConstructor;
 import nycto.homeservices.dto.subService.SubServiceCreateDto;
 import nycto.homeservices.dto.subService.SubServiceResponseDto;
 import nycto.homeservices.entity.SubService;
+import nycto.homeservices.exceptions.NotFoundException;
 import nycto.homeservices.exceptions.NotValidInputException;
 import nycto.homeservices.repository.SubServiceRepository;
 import nycto.homeservices.util.ValidationUtil;
 import nycto.homeservices.util.dtoMapper.SubServiceMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +29,22 @@ public class SubServiceService {
         SubService subService = subServiceMapper.toEntity(dto, excitingService);
 
         return subServiceMapper.toResponseDto(subServiceRepository.save(subService));
+    }
+
+    public SubServiceResponseDto getSubServiceById(Long id)
+            throws NotFoundException {
+
+        SubService subService = subServiceRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("SubService with id "
+                        + id + " not found"));
+
+        return subServiceMapper.toResponseDto(subService);
+    }
+
+    public List<SubServiceResponseDto> getAllSubServices() {
+        return subServiceRepository.findAll().stream()
+                .map(subServiceMapper::toResponseDto)
+                .collect(Collectors.toList());
     }
 
 }
