d01a78e96365a887220d5925b358fefaca1232b5
Sajad <nyctoxr@gmail.com>
2025-04-02 23:49:14 +0330

    Implemented ServiceController with createService, getServiceById, and getAllServices methods.

--- src/main/java/nycto/homeservices/controller/ServiceController.java
+++ src/main/java/nycto/homeservices/controller/ServiceController.java
@@ -1,12 +1,40 @@
 package nycto.homeservices.controller;
 
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import nycto.homeservices.dto.serviceDto.ServiceCreateDto;
+import nycto.homeservices.dto.serviceDto.ServiceResponseDto;
+import nycto.homeservices.service.serviceInterface.ServiceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/services")
 @RequiredArgsConstructor
 public class ServiceController {
+
+    private final ServiceService serviceService;
+
+    @PostMapping
+    public ResponseEntity<ServiceResponseDto> createService(@Valid @RequestBody ServiceCreateDto createDto) {
+        ServiceResponseDto responseDto = serviceService.createService(createDto);
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ServiceResponseDto> getServiceById(@PathVariable Long id) {
+        ServiceResponseDto responseDto = serviceService.getServiceById(id);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ServiceResponseDto>> getAllServices() {
+        List<ServiceResponseDto> services = serviceService.getAllServices();
+        return ResponseEntity.ok(services);
+    }
+
 }
